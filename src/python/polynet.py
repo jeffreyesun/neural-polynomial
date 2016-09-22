1de496da8e9b0ac9e373f8edf8c4ec7fa9c5b39c
jeffreyesun <jeffreyesun@gmail.com>
2016-09-22 16:24:25 -0400

    Removed duplicate and test files

--- src/python/polynet.py
+++ src/python/polynet.py
@@ -1,5 +1,4 @@
 import random
-
 import numpy as np
 
 class Network():
@@ -35,12 +34,10 @@ class Network():
         self.training_data = training_data
         for generation in xrange(generations):
             self.distribute(generation + 1, generations, tests_per_point, points_per_generation)
+            print "Finished generation {0}".format(generation)
             if test_data:
                 print "{0}. Connection distance from origin: {1}".format(
                        self.evaluate(test_data), str(sum(sum(sum(self.connections*self.connections)))))
-            else:
-                print "Finished generation {0}".format(generation)
-
 
     def distribute(self, generation, generations, tests_per_point, points):
         total_weight = np.zeros([self.outputs, 1])
@@ -82,41 +79,11 @@ class Network():
 
     def evaluate(self, test_data):
         test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
-        
-        return str((sum(int(x == y) for (x, y) in test_results), sum(int(x == 0) for (x, y) in test_results), sum(int(x == 1) for (x, y) in test_results), sum(int(x == 2) for (x, y) in test_results), sum(int(x == 3) for (x, y) in test_results), sum(int(x == 4) for (x, y) in test_results), sum(int(x == 5) for (x, y) in test_results), sum(int(x == 6) for (x, y) in test_results), sum(int(x == 7) for (x, y) in test_results), sum(int(x == 8) for (x, y) in test_results), sum(int(x == 9) for (x, y) in test_results)))
+        number_correct = sum(int(x == y) for (x, y) in test_results)
+	total_tests = len(test_results)
+	accuracy = float(number_correct)/total_tests
+        analysis = "{0} correct out of {1}. Accuracy {2}%".format(number_correct, total_tests, accuracy*100)
+        return(analysis)
 
     def genrandom(self):
         return np.random.randn(self.degree, self.outputs, self.inputs)/40 #/(100+ 10*generation) + self.connections #TODO: change distribution
-
-
-
-
-'''
-    def ddistribute(self, training_data, generation, dist_batch_size, test_data):
-        random.shuffle(training_data)
-
-        #print(len(training_data))
-        #print(dist_batch_size)
-
-        for candidate in xrange(int(len(training_data)/dist_batch_size)):
-            self.weigh(training_data, candidate, generation, dist_batch_size, test_data)
-
-    def weighh(self, training_data, candidate, generation, dist_batch_size, test_data):
-
-        random_net = self.genrandom(generation)
-        start = candidate*dist_batch_size
-        stop = start + dist_batch_size
-
-        #print("distance:         :" + str(sum(sum(sum(random_net*random_net)))))
-
-        weight = sum([self.cost(random_net, *training_data[point]) for point in xrange (start, stop)])
-        self.connections += 1000*(random_net-self.connections)/(weight*weight*dist_batch_size)
-        #Expensive, and how much to move the connections?
-        #print(100/(weight*weight))
-
-
-    def cost(self, random_net, input, output):
-        difference = prediction - output
-        cost = np.sum(difference*difference)/len(output)
-        return cost
-'''
